9cb76e284546c1e153151dcbee11ad3086795cf1
yeugene <rainbird9@gmail.com>
2025-11-20 22:18:57 +0200

    Apply formatter

--- test/subject_manager_web/controllers/error_html_test.exs
+++ test/subject_manager_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule SubjectManagerWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(SubjectManagerWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(SubjectManagerWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
--- test/subject_manager_web/controllers/error_json_test.exs
+++ test/subject_manager_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule SubjectManagerWeb.ErrorJSONTest do
   use SubjectManagerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert SubjectManagerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert SubjectManagerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
