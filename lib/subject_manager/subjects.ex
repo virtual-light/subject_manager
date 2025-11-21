55be2ab726237bd0b6ff509b273f5c9fbacd1e77
yeugene <rainbird9@gmail.com>
2025-11-21 19:21:12 +0200

    Add filtering and ordering for /subjects

--- lib/subject_manager/subjects.ex
+++ lib/subject_manager/subjects.ex
@@ -2,6 +2,8 @@ defmodule SubjectManager.Subjects do
   alias SubjectManager.Subjects.Subject
   alias SubjectManager.Repo
 
+  import Ecto.Query
+
   @type sort_by :: :name | :team | :position
   @type params :: %{
           q: String.t() | nil,
@@ -10,7 +12,47 @@ defmodule SubjectManager.Subjects do
         }
 
   @spec list_subjects(params()) :: [Subject.t()]
-  def list_subjects(_params) do
-    Repo.all(Subject)
+  def list_subjects(params) do
+    from(s in Subject)
+    |> apply_params(params)
+    |> Repo.all()
+  end
+
+  defp apply_params(query, params) do
+    query
+    |> maybe_filter(params)
+    |> maybe_order_by(params.sort_by)
+  end
+
+  defp maybe_filter(query, params) do
+    where =
+      true
+      |> maybe_filter_by_position(params.position)
+      |> maybe_filter_by_name(params.q)
+
+    from query, where: ^where
+  end
+
+  defp maybe_filter_by_position(where, position) do
+    if is_nil(position) do
+      where
+    else
+      dynamic([s], s.position == ^position and ^where)
+    end
+  end
+
+  defp maybe_filter_by_name(where, name) do
+    if is_nil(name) do
+      where
+    else
+      name = "%#{name}%"
+      dynamic([s], like(s.name, ^name) and ^where)
+    end
+  end
+
+  defp maybe_order_by(query, nil), do: query
+
+  defp maybe_order_by(query, order_by) do
+    from s in query, order_by: ^order_by
   end
 end
--- test/subject_manager_web/live/subject_live_test.exs
+++ test/subject_manager_web/live/subject_live_test.exs
@@ -16,6 +16,129 @@ defmodule SubjectManagerWeb.SubjectLiveTest do
       assert MapSet.new(subjects, &Map.take(&1, [:name, :team, :position])) ==
                MapSet.new(parsed_sbujects)
     end
+
+    test "filtered by position", %{conn: conn} do
+      position = :defender
+
+      insert_subject!()
+      insert_subject!(position: position)
+
+      conn = get(conn, "/subjects", position: Atom.to_string(position))
+      {:ok, _view, html} = live(conn)
+
+      assert [%{position: ^position}] = parse_subjects(html)
+    end
+
+    test "filtered by name", %{conn: conn} do
+      name = "FindMe"
+
+      insert_subject!()
+      insert_subject!(name: name)
+
+      conn = get(conn, "/subjects", q: name)
+      {:ok, _view, html} = live(conn)
+
+      assert [%{name: ^name}] = parse_subjects(html)
+    end
+
+    test "search by name", %{conn: conn} do
+      insert_subject!()
+      subject1 = insert_subject!(name: "John Doe")
+      subject2 = insert_subject!(name: "Marry Doe The Second")
+
+      conn = get(conn, "/subjects", q: "Doe")
+      {:ok, _view, html} = live(conn)
+
+      parsed_subjects = parse_subjects(html)
+
+      subjects = [subject1, subject2]
+
+      assert MapSet.new(subjects, & &1.name) == MapSet.new(parsed_subjects, & &1.name)
+    end
+
+    test "filtered exclusively by name and position", %{conn: conn} do
+      name = "FindMe"
+      position = :winger
+
+      insert_subject!()
+      insert_subject!(name: name)
+      insert_subject!(position: position)
+      insert_subject!(name: name, position: position)
+
+      conn = get(conn, "/subjects", q: name, position: Atom.to_string(position))
+      {:ok, _view, html} = live(conn)
+
+      assert [%{name: ^name, position: ^position}] = parse_subjects(html)
+    end
+
+    test "ordered by name", %{conn: conn} do
+      names = ["Curtis", "Zohran", "Andrew"]
+      Enum.each(names, &insert_subject!(name: &1))
+
+      conn = get(conn, "/subjects", sort_by: "name")
+      {:ok, _view, html} = live(conn)
+
+      parsed_subjects = parse_subjects(html)
+      assert Enum.map(parsed_subjects, & &1.name) == Enum.sort(names)
+    end
+
+    test "ordered by team", %{conn: conn} do
+      teams = ["Red", "Blue", "Purple"]
+      Enum.each(teams, &insert_subject!(team: &1))
+
+      conn = get(conn, "/subjects", sort_by: "team")
+      {:ok, _view, html} = live(conn)
+
+      parsed_subjects = parse_subjects(html)
+      assert Enum.map(parsed_subjects, & &1.team) == Enum.sort(teams)
+    end
+
+    test "ordered by position", %{conn: conn} do
+      positions = [:defender, :winger]
+      Enum.each(positions, &insert_subject!(position: &1))
+
+      conn = get(conn, "/subjects", sort_by: "position")
+      {:ok, _view, html} = live(conn)
+
+      parsed_subjects = parse_subjects(html)
+      assert Enum.map(parsed_subjects, & &1.position) == Enum.sort(positions)
+    end
+
+    test "filtered by name and ordered by position", %{conn: conn} do
+      insert_subject!()
+      params = [%{name: "John Doe", position: :defender}, %{name: "John Cena", position: :winger}]
+
+      Enum.each(params, &insert_subject!(Keyword.new(&1)))
+
+      conn = get(conn, "/subjects", sort_by: "position", q: "John")
+      {:ok, _view, html} = live(conn)
+
+      parsed_subjects = parse_subjects(html)
+
+      assert Enum.map(parsed_subjects, &Map.take(&1, [:name, :position])) ==
+               Enum.sort_by(params, & &1.position)
+    end
+
+    test "empty on incorrect position query param", %{conn: conn} do
+      insert_subject!()
+      insert_subject!(position: :defender)
+
+      conn = get(conn, "/subjects", position: "wrong_position_name")
+
+      {:ok, _view, html} = live(conn)
+
+      assert [] = parse_subjects(html)
+    end
+
+    test "empty on incorrect sort_by query param", %{conn: conn} do
+      insert_subject!()
+
+      conn = get(conn, "/subjects", sort_by: "wrong_sort")
+
+      {:ok, _view, html} = live(conn)
+
+      assert [] = parse_subjects(html)
+    end
   end
 
   defp parse_subjects(html) do
@@ -33,8 +156,8 @@ defmodule SubjectManagerWeb.SubjectLiveTest do
     end)
   end
 
-  defp insert_subject!(opts \\ []) do
-    subject = Keyword.get_lazy(opts, :subject, &subject/0)
+  defp insert_subject!(opts \\ nil) do
+    subject = if is_nil(opts), do: subject(), else: subject(opts)
 
     %Subject{}
     |> Ecto.Changeset.change(subject)
